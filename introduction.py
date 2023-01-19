e1064c10102c0ee05aa269e287516302f66bc89e
Suraj S <113158658+surajsureshkumar@users.noreply.github.com>
2023-01-19 08:39:55 -0500

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,33 @@
+import math
+
+print("Hello World")
+# Arithmetic Operation on 3 and 4
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Printing Strings
+print('ABC')
+print('S')
+print('XYZ')
+print('I am enjoying learning python')
+
+# Checking data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['ABC']))
+print(type('Rampage'))
+print(type('This is fun'))
+print(type('United States of America'))
+
+# Creating two lists
+point1 = [2, 3]
+point2 = [10, 8]
+# Using python inbuilt math class to calculate the Eucledian distance
+print(math.dist(point1, point2))
